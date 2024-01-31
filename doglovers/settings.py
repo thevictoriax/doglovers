fa4f823c80aaaa3d61157572f059e798d1d92bb0
thevictoriax <viktoriia.makukh@lnu.edu.ua>
2024-01-31 14:31:28 +0200

    db connect

--- doglovers/settings.py
+++ doglovers/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'doglovers.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DogLovers',
+        'USER': 'postgres',
+        'PASSWORD': 'Vitavika2004',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
