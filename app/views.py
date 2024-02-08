0ad57b30d21d36ad66ab882573f59334f77207c4
thevictoriax <viktoriia.makukh@lnu.edu.ua>
2024-02-08 18:03:59 +0200

    refresh comment bug fixed

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from app.models import Post
+from app.models import Post, Comments
 from app.forms import CommentForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -10,6 +12,7 @@ def index(request):
 
 def post_page(request, slug):
     post = get_object_or_404(Post, slug=slug)
+    comments = Comments.objects.filter(post=post)
     form = CommentForm()
 
     if request.method == 'POST':
@@ -20,6 +23,7 @@ def post_page(request, slug):
             post = Post.objects.get(id=postid)
             comment.post = post
             comment.save()
+            return HttpResponseRedirect(reverse('post_page', kwargs={'slug':slug}))
 
         if post.view_count is None:
             post.view_count = 1
@@ -27,5 +31,5 @@ def post_page(request, slug):
             post.view_count += 1
         post.save()
 
-    context = {'post': post, 'form': form}
+    context = {'post': post, 'form': form, 'comments':comments}
     return render(request, 'app/post.html', context)
