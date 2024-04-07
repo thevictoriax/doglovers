8785db668a50535218d0389a2f3fa9214ac01cd0
thevictoriax <viktoriia.makukh@lnu.edu.ua>
2024-04-07 19:16:24 +0300

    likes added

--- app/urls.py
+++ app/urls.py
@@ -12,7 +12,8 @@ urlpatterns = [
    path('search/', views.search_posts, name='search'),
    path('about/', views.about, name='about'),
    path('accounts/register/', views.register_user, name='register'),
-   path('bookmark_post/<slug:slug>', views.bookmark_post, name='bookmark_post')
+   path('bookmark_post/<slug:slug>', views.bookmark_post, name='bookmark_post'),
+   path('like_post/<slug:slug>', views.like_post, name='like_post')
 
 
 ]
--- app/views.py
+++ app/views.py
@@ -44,6 +44,12 @@ def post_page(request, slug):
         bookmarked = True
     is_bookmarked = bookmarked
 
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        liked = True
+    number_of_likes = post.number_of_likes()
+    post_is_liked = liked
+
     if request.POST:
         comment_form = CommentForm(request.POST)
         if comment_form.is_valid:
@@ -72,7 +78,7 @@ def post_page(request, slug):
         post.view_count = post.view_count + 1
     post.save()
     
-    context = {'post':post, 'form':form, 'comments':comments, 'is_bookmarked':is_bookmarked}
+    context = {'post':post, 'form':form, 'comments':comments, 'is_bookmarked':is_bookmarked, 'post_is_liked':  post_is_liked, 'number_of_likes':number_of_likes}
     return render(request, 'app/post.html', context)
 
 def tag_page(request, slug):
@@ -129,4 +135,13 @@ def bookmark_post(request, slug):
         post.bookmarks.remove(request.user)
     else:
         post.bookmarks.add(request.user)
+    return HttpResponseRedirect(reverse('post_page', args=[str(slug)]))
+
+def like_post(request, slug):
+    print("PRINT", request.POST.get('post_id'))
+    post = get_object_or_404(Post, id=request.POST.get('post_id'))
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+    else:
+        post.likes.add(request.user)
     return HttpResponseRedirect(reverse('post_page', args=[str(slug)]))
