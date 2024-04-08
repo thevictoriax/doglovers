d82f01cc755c7de1f9fd06d32bcadd6ed0b57482
thevictoriax <viktoriia.makukh@lnu.edu.ua>
2024-04-08 18:13:25 +0300

    all posts added

--- app/views.py
+++ app/views.py
@@ -150,3 +150,10 @@ def all_bookmarked_posts(request):
     all_bookmarked_posts = Post.objects.filter(bookmarks = request.user)
     context = {'all_bookmarked_posts':all_bookmarked_posts}
     return render(request, 'app/all_bookmarked_posts.html', context)
+
+
+def all_posts(request):
+    all_posts = Post.objects.all()
+    context = {'all_posts':all_posts}
+    return render(request, 'app/all_posts.html', context)
+
