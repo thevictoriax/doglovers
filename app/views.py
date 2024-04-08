011843d347340a2e487ce24b539ef21beafb1366
thevictoriax <viktoriia.makukh@lnu.edu.ua>
2024-04-08 18:40:37 +0300

    footer fixed

--- app/views.py
+++ app/views.py
@@ -157,3 +157,7 @@ def all_posts(request):
     context = {'all_posts':all_posts}
     return render(request, 'app/all_posts.html', context)
 
+def all_liked_posts(request):
+    all_liked_posts = Post.objects.filter(likes = request.user)
+    context = {'all_liked_posts':all_liked_posts}
+    return render(request, 'app/all_liked_posts.html', context)
